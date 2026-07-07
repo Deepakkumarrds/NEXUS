7a4c6f9817e72c1b29cda6185f3c47781e9d007e
Gowtham <ktgowtham89@gmail.com>
2026-07-07 18:10:46 +0530

    chore: remove unwanted test and seed scripts

--- backend/check_db.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const users = await prisma.user.findMany();
-  console.log('Users in DB:', users.length);
-  const clients = await prisma.client.findMany();
-  console.log('Clients in DB:', clients.length);
-}
-main().catch(e => console.error(e)).finally(() => prisma.$disconnect());
